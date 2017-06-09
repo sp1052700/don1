764c320acd8c5ae8ebc0f711aba284b08b5095e0
Sp1052800 <sp1052800@gmail.com>
2017-06-09 12:38:38 +0530

    first commit

---
+++ app/views/users/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @user, :id, :name, :email, :city, :mobile, :password, :created_at, :updated_at
---
+++ db/migrate/20170609070707_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :city
+      t.integer :mobile
+      t.string :password
+
+      t.timestamps
+    end
+  end
+end
